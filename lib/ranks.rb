ad01dcbec70c7192ba1ee1d535bdef13ba269421
Patrick Carver <patrickcarver@gmail.com>
2016-04-14 23:09:35 -0500

    created module for rank values

---
+++ lib/ranks.rb
@@ -0,0 +1,18 @@
+module PatrickCarver
+  module ProjectEuler
+    module Problem54
+      module Ranks
+        HIGH_CARD = 1
+        ONE_PAIR = 2
+        TWO_PAIRS = 3
+        THREE_OF_A_KIND = 4
+        STRAIGHT = 5
+        FLUSH = 6
+        FULL_HOUSE = 7
+        FOUR_OF_A_KIND = 8
+        STRAIGHT_FLUSH = 9
+        ROYAL_FLUSH = 10
+      end
+    end
+  end
+end
